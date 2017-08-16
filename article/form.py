a5adcd8ebb4a72142d77de1f6253ae50adcc6fff
demm <e-egoza@mail.ru>
2017-08-16 18:26:50 +0700

    !!

--- article/form.py
+++ article/form.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class ContactForm(forms.Form):
-    subject = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'size':'40','class': 'modal-write__input','placeholder': 'subject'}))
+    subject = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'id': 'post-text', 'required': True,'size':'40','class': 'modal-write__input','placeholder': 'subject'}))
     sender = forms.EmailField(widget=forms.TextInput(attrs={'size':'40','class': 'modal-write__input'}))
     #message = forms.CharField(widget=forms.Textarea(attrs={'class': 'modal-write__input'}))
     #copy = forms.BooleanField(required=False)
